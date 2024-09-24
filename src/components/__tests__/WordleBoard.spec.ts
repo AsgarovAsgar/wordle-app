2ae56eaff205e9b5ba335243b7fa33affd178828
Asgar Asgarov <asgarovasgar28@gmail.com>
2024-09-24 15:38:29 +0400

    enforce the word of the day is the real English word

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -53,4 +53,12 @@ describe('WordleBoard', () => {
 
     expect(console.warn).toHaveBeenCalled()
   })
+
+  test('if the word of the day is not a real English word, a warning is emitted', async () => {
+    console.warn = vi.fn()
+
+    mount(WordleBoard, {props: {wordOfTheDay: 'ZZZZZ'}})
+
+    expect(console.warn).toHaveBeenCalled()
+  })
 })
